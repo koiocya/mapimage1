79ea1595e40bfcc46244ac2218208ea9b9abbbeb
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-09 16:21:23 +0900

    ユーザー編集機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,31 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #　以下：ユーザー編集機能
+  
+  def update_without_current_password(params, *options)
+     params.delete(:current_password)
 
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
+
+  #　以下：ユーザー登録機能の制限
   validates :nickname, presence: true 
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, { with: PASSWORD_REGEX, message: '半角英数字で入力して下さい' }
     validates :password, presence: true, length: { minimum: 8 }
 
+  #　以下：アソシエーション
   has_many :tweets
+
+  #　以下：ユーザー画像機能
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_060313) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "introduction", null: false
+    t.string "image", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
