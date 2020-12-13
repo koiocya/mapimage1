623387d5b8de64f4cc89f29d41e9cfbd61891bfd
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-13 12:08:48 +0900

    プレビュー機能

---
+++ app/javascript/new.js
@@ -0,0 +1,23 @@
+if (document.URL.match( /sign_up/ ) || document.URL.match( /edit/ ) || document.URL.match( /new/ )) {
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list');
+  document.getElementById('tweet_image').addEventListener('change', function(e){
+
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+
+  });
+});
+}
