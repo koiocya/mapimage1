b8a71f6588b5c7bb3519a761404ea5fd079aebe9
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-15 14:23:51 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,13 @@ Rails.application.routes.draw do
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   resources :maps
   get '/map_request', to: 'maps#map', as: 'map_request'
 >>>>>>> parent of afd3755... 緯度経度を使用しgooglemapに反映
+=======
+>>>>>>> parent of 9002a1e... Merge pull request #17 from koiocya/revert-16-revert-15-googlemap連携
 =======
 >>>>>>> parent of 9002a1e... Merge pull request #17 from koiocya/revert-16-revert-15-googlemap連携
 =======
