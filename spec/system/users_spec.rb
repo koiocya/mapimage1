9b5cc2f0ce842b678899d24bf0af8df2cf72db63
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-18 11:23:41 +0900

    ユーザーが新規登録できないテストコード

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -14,12 +14,8 @@ RSpec.describe "Users", type: :system do
       #新規登録ページへ移動する
       visit new_user_registration_path
       #ユーザー情報を入力する
-      # expect(page).to have_content('プロフィール画像を選択')
       image_path = Rails.root.join('spec/fixtures/test.jpg')
       attach_file('user[image]', image_path, make_visible: true)
-      # expect{
-        
-      # }
       fill_in 'ニックネーム', with: @user.nickname
       fill_in 'メールアドレス', with: @user.email
       fill_in 'パスワード', with: @user.password
@@ -30,12 +26,34 @@ RSpec.describe "Users", type: :system do
       }.to change { User.count }.by(1)
       # トップページへ遷移する
       expect(current_path).to eq root_path
-      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      # トップページにログアウトボタンが表示されることを確認する
       expect(page).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
   end
-
+  context 'ユーザーが新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページに戻ってくる' do
+      #トップページに移動する
+      visit root_path
+      #トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      #新規登録ページへ移動する
+      visit new_user_registration_path
+      #ユーザー情報を入力する
+      image_path = Rails.root.join("")
+      attach_file('user[image]', image_path, make_visible: true)
+      fill_in 'ニックネーム', with: ""
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: ""
+      fill_in 'パスワード(確認)', with: ""
+      #登録ボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="signup"]').click
+      }.to change {User.count }.by(0)
+      #新規登録ページに戻されることを確認する
+      expect(current_path).to eq "/users"
+    end
+  end
 end
