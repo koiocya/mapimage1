da9072d11682ffe2cab6ba27362bd1009b431653
koiocya <73815463+koiocya@users.noreply.github.com>
2020-12-20 00:54:07 +0900

    コメントモデルのテストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    @tweet = FactoryBot.build(:tweet)
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの保存' do
+    context 'コメントが保存できる場合' do
+      it "テキストがあればコメントは保存される" do
+        expect(@user).to be_valid
+        expect(@tweet).to be_valid
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメントが保存できない場合' do
+      it "テキストがないとコメントは保存されない" do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("テキストを入力してください")
+      end
+    end
+  end
 end
